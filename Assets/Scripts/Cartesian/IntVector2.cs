460c9ef93268c62b7da00698022d5c90df8f761f
Kyle <ismakefire@gmail.com>
2016-04-21 14:26:46 -0700

    Based on some research the most common implementation, along side what I'll be working with.

---
+++ Assets/Scripts/Cartesian/IntVector2.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Original author Kyle Misner
+ * GitHub: https://github.com/ismakefire
+ * 
+ * An integer version of Unity's Vector2 class.
+ */
+
+namespace Cartesian {
+	[System.Serializable]
+	public struct IntVector2 {
+
+		public int x;
+		public int y;
+
+		/// <summary>
+		/// Constructs a new vector with given x, y components.
+		/// </summary>
+		public IntVector2 (int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+
+		public static IntVector2 operator +(IntVector2 left, IntVector2 right) {
+			left.x += right.x;
+			left.y += right.y;
+			return left;
+		}
+	}
+}
---
+++ Assets/Scripts/Popular/IntVector2.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Original author Kyle Misner
+ * GitHub: https://github.com/ismakefire
+ * 
+ * An integer version of Unity's Vector2 class, based on common useage in GitHub. Probably due to this tutorial: http://catlikecoding.com/unity/tutorials/maze/
+ */
+
+namespace Popular {
+	[System.Serializable]
+	public struct IntVector2 {
+
+		public int x;
+		public int z;
+
+		/// <summary>
+		/// Constructs a new vector with given x, z components.
+		/// </summary>
+		public IntVector2 (int x, int z) {
+			this.x = x;
+			this.z = z;
+		}
+
+		public static IntVector2 operator +(IntVector2 a, IntVector2 b) {
+			a.x += b.x;
+			a.z += b.z;
+			return a;
+		}
+	}
+}
